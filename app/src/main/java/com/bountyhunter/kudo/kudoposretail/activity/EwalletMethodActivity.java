c577e5b46cfa95e123b78e3b34b45ddea5ccb9d8
Norman Lie <norman.lie@kudo.co.id>
2017-11-17 17:08:53 +0700

    Create activity for ewallet payment

---
+++ app/src/main/java/com/bountyhunter/kudo/kudoposretail/activity/EwalletMethodActivity.java
@@ -0,0 +1,17 @@
+package com.bountyhunter.kudo.kudoposretail.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bountyhunter.kudo.kudoposretail.R;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_ewallet_method)
+public class EwalletMethodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
