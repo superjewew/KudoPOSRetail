927d8ca31d9ac3373a3bae6190db78cbb9f32918
Norman Lie <norman.lie@kudo.co.id>
2017-11-17 11:38:43 +0700

    Create wangpos sdk wrapper class

---
+++ app/src/main/java/com/bountyhunter/kudo/kudoposretail/Wangpos.java
@@ -0,0 +1,57 @@
+package com.bountyhunter.kudo.kudoposretail;
+
+import android.content.Context;
+
+import sdk4.wangpos.libemvbinder.EmvCore;
+import wangpos.sdk4.libbasebinder.BankCard;
+import wangpos.sdk4.libbasebinder.Core;
+
+/**
+ * Created by norman on 11/17/17.
+ */
+
+public class Wangpos {
+
+    public static Wangpos sInstance;
+    private Context mContext;
+    private Core mBaseCore;
+    private BankCard mBankCard;
+    private EmvCore mEmvCore;
+    private boolean mDoneInit;
+
+    private Wangpos(Context context) {
+        mContext = context;
+    }
+
+    public static Wangpos getInstance(Context context) {
+        if (sInstance == null) {
+            sInstance = new Wangpos(context.getApplicationContext());
+        }
+
+        return sInstance;
+    }
+
+    public void setupWangposCores() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                setupBaseCore(mContext);
+                setupEmvCore(mContext);
+                setupBankCard(mContext);
+                mDoneInit = true;
+            }
+        }).start();
+    }
+
+    private void setupBaseCore(Context context) {
+        mBaseCore = new Core(context);
+    }
+
+    private void setupEmvCore(Context context) {
+        mEmvCore = new EmvCore(context);
+    }
+
+    private void setupBankCard(Context context) {
+        mBankCard = new BankCard(context);
+    }
+}
