377a4a98a883ad0dd1f310d4fc932615b5ff7cba
Norman Lie <norman.lie@kudo.co.id>
2017-11-17 11:38:13 +0700

    Create card payment method screen

---
+++ app/src/main/java/com/bountyhunter/kudo/kudoposretail/CardMethodActivity.java
@@ -0,0 +1,15 @@
+package com.bountyhunter.kudo.kudoposretail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_card_method)
+public class CardMethodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
