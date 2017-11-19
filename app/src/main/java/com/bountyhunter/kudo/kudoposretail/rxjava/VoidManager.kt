70f778cb1c4238c7e4be950de4fe42c11b1d4bd3
Norman Lie <norman.lie@kudo.co.id>
2017-11-19 16:21:54 +0700

    Create manager for executing void API

---
+++ app/src/main/java/com/bountyhunter/kudo/kudoposretail/rxjava/VoidManager.kt
@@ -0,0 +1,27 @@
+package com.bountyhunter.kudo.kudoposretail.rxjava
+
+import com.bountyhunter.kudo.kudoposretail.api.RestAPI
+import com.bountyhunter.kudo.kudoposretail.api.VoidResponse
+import rx.Observable
+
+/**
+ * Created by norman on 11/19/17.
+ */
+class VoidManager {
+
+    fun void(transactionNumber: String, pin : Int) : Observable<VoidResponse> {
+        return Observable.create { subscriber ->
+            run {
+                val restAPI = RestAPI()
+                val callResponse = restAPI.void(transactionNumber, pin);
+                val response = callResponse.execute()
+                if(response.isSuccessful) {
+
+                } else {
+                    subscriber.onError(Throwable("Gagal membatalkan transaksi"))
+                }
+                subscriber.onCompleted()
+            }
+        }
+    }
+}
