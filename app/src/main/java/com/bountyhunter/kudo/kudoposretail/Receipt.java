c34864f475edcfc62e3cfb1a5729f0ce80c02c3d
Norman Lie <norman.lie@kudo.co.id>
2017-11-19 14:11:52 +0700

    Refactor receipt to show product list

--- app/src/main/java/com/bountyhunter/kudo/kudoposretail/Receipt.java
+++ app/src/main/java/com/bountyhunter/kudo/kudoposretail/Receipt.java
@@ -1,8 +1,11 @@
 package com.bountyhunter.kudo.kudoposretail;
 
+import android.util.Log;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
 
 import wangpos.sdk4.libbasebinder.Printer;
@@ -14,11 +17,16 @@ import wangpos.sdk4.libbasebinder.Printer;
 public class Receipt {
     List<ReceiptString> mContents = new ArrayList<>();
 
-    public Receipt(int totalPrice, int method) {
+    public Receipt(HashMap<String, Integer> products, int totalPrice, int method) {
         generateHeader();
         generateTimeAndDate();
+        generateItems(products);
         generateTotalPrice(totalPrice);
         generatePaymentMethod(method);
+
+        for (ReceiptString string: mContents) {
+            Log.d("RECEIPT", string.getContent());
+        }
     }
 
     public List<ReceiptString> getContents() {
@@ -33,6 +41,10 @@ public class Receipt {
         header.add(new ReceiptString("Kudoplex2", 24, Printer.Align.LEFT, false, false));
         header.add(new ReceiptString("Radio Dalam", 24, Printer.Align.LEFT, false, false));
         header.add(new ReceiptString("Jaksel", 24, Printer.Align.LEFT, false, false));
+        header.add(new ReceiptString("--------------------------------------------", 30, Printer.Align.CENTER, false, false));
+        header.add(new ReceiptString(" ", 30, Printer.Align.CENTER, false, false));
+        header.add(new ReceiptString("Pembayaran Sukses", 30, Printer.Align.CENTER, false, false));
+        header.add(new ReceiptString(" ", 30, Printer.Align.CENTER, false, false));
 
         mContents.addAll(header);
     }
@@ -44,6 +56,17 @@ public class Receipt {
         mContents.add(new ReceiptString(format.format(cal.getTime()), 24, Printer.Align.LEFT, false, false));
     }
 
+    private void generateItems(HashMap<String, Integer> productMap) {
+        List<ReceiptString> products = new ArrayList<>();
+
+        for(String productName : productMap.keySet()) {
+            products.add(new ReceiptString(productName + " : " + productMap.get(productName), 24, Printer.Align.LEFT, false, false));
+        }
+        products.add(new ReceiptString(" ", 30, Printer.Align.LEFT, false, false));
+
+        mContents.addAll(products);
+    }
+
     private void generateTotalPrice(int price) {
         mContents.add(new ReceiptString("Total: " + price, 24, Printer.Align.LEFT, false, false));
     }
@@ -53,14 +76,14 @@ public class Receipt {
             case 0:
                 break;
             case 1:
-                List<ReceiptString> header = new ArrayList<>();
+                List<ReceiptString> footer = new ArrayList<>();
 
-                header.add(new ReceiptString("Approved", 24, Printer.Align.LEFT, true, false));
-                header.add(new ReceiptString("Card No 1234 XXXX XXXX XXXX", 24, Printer.Align.LEFT, false, false));
-                header.add(new ReceiptString("Card Type VISA CREDIT", 24, Printer.Align.LEFT, false, false));
-                header.add(new ReceiptString("Trace No XXXXXX", 24, Printer.Align.LEFT, false, false));
+                footer.add(new ReceiptString("Approved", 24, Printer.Align.LEFT, true, false));
+                footer.add(new ReceiptString("Card No 1234 XXXX XXXX XXXX", 24, Printer.Align.LEFT, false, false));
+                footer.add(new ReceiptString("Card Type VISA CREDIT", 24, Printer.Align.LEFT, false, false));
+                footer.add(new ReceiptString("Trace No XXXXXX", 24, Printer.Align.LEFT, false, false));
 
-                mContents.addAll(header);
+                mContents.addAll(footer);
                 break;
             case 2:
                 break;
