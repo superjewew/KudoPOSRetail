e0c998be30388abd127bd4a60590bb394c845839
Norman Lie <norman.lie@kudo.co.id>
2017-11-22 14:31:06 +0700

    Edit cart item data structure

--- app/src/main/java/com/bountyhunter/kudo/kudoposretail/model/CartItem.kt
+++ app/src/main/java/com/bountyhunter/kudo/kudoposretail/model/CartItem.kt
@@ -1,16 +1,15 @@
 package com.bountyhunter.kudo.kudoposretail.model
 
 import io.realm.RealmObject
-import io.realm.annotations.PrimaryKey
 
 /**
  * Created by norman on 11/21/17.
  */
 open class CartItem : RealmObject() {
-    @PrimaryKey
-    var id: Long? = 0
     var mItemId: Long? = 0
     var mItemName: String? = ""
     var mItemQuantity: Int = 0
-    var mItemPrice: Long = 0
+    var mItemPrice: Double = 0.0
+    var mItemImage: String = "No Image"
+    var mItemStock: Int = 10
 }
