8ade57ad78b7ecca9079b56222a9e2c284e08afb
Norman Lie <norman.lie@kudo.co.id>
2017-11-23 12:16:19 +0700

    Create process transaction screen

---
+++ app/src/main/java/com/bountyhunter/kudo/kudoposretail/activity/ProcessTransactionActivity.kt
@@ -0,0 +1,13 @@
+package com.bountyhunter.kudo.kudoposretail.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.bountyhunter.kudo.kudoposretail.R
+
+class ProcessTransactionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_process_transaction)
+    }
+}
